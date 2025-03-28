7c342c279f4b31d176c2dfd76de59e978387ba7c
nadunlupr <nadun.liyanage@famnit.upr.si>
2025-03-28 17:02:24 +0100

    28-03-2025 class changes

---
+++ src/AppRoutes.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import LessonDetails from './components/LessonDetails';
+import App from './App';
+
+class AppRoutes extends Component {
+    render() {
+        return(
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="lesson" element={<LessonDetails />} />
+                </Routes>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default AppRoutes;
--- src/components/LessonCard.js
+++ src/components/LessonCard.js
@@ -1,9 +1,14 @@
 import { Component } from 'react';
+import axios from 'axios';
+import loaderImg from '../assets/images/loader.gif';
+import { Link } from 'react-router';
+
 class LessonCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showMore: false
+            showMore: false,
+            description: undefined
         };
     }
     render() {
@@ -15,22 +20,55 @@ class LessonCard extends Component {
                         {this.props.description}
                     </p>
                 </div>
-                <button onClick={() => this
-                    .setState({ showMore: !this.state.showMore })}>
+                <button onClick={() => this.onViewSummaryClicked()}>
                     View Summary
                 </button>
                 {
                     this.state.showMore &&
-                    <div id="moreInfo"
-                        className="more-info-panel">
-                        <p className="black-text">
-                            If you take the first lesson you can do the second one :)
-                        </p>
+                    <div>
+                        {
+                            this.state.description ?
+                                <div id="moreInfo"
+                                    className="more-info-panel">
+                                    <p className="black-text">
+                                        {this.state.description}
+                                    </p>
+                                </div> :
+                                <div>
+                                    <img src={loaderImg} width="40px" alt="loading..." />
+                                </div>
+                        }
+                    <Link to="/lesson" >Go to Lesson</Link>
                     </div>
+                    
                 }
 
             </div>
         );
+    }
+
+    onViewSummaryClicked() {
+        this.setState({ showMore: !this.state.showMore });
+        this.getDetails();
+    }
+
+    getDetails() {
+        axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?2015/summer/cmpt/120/${this.props.name}`)
+            .then(res => {
+                const {
+                    data: {
+                        info: {
+                            description
+                        } = {}
+                    } = {}
+                } = res;
+                console.log('result', description);
+                this.setState(prevState => ({
+                    ...prevState,
+                    description
+                }))
+            })
+            .catch(err => console.log('error loading data: ', err));
     }
 }
 
