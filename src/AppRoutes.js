058a9ec925ca05d4be509c1ce98eb8006e3d86b9
nadunlupr <nadun.liyanage@famnit.upr.si>
2025-04-11 17:10:29 +0200

    intro functional components, use state, axios config

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import LessonDetails from './components/LessonDetails';
 import App from './App';
+import CreateNews from './components/news/CreateNews';
 
 class AppRoutes extends Component {
     render() {
@@ -10,6 +11,7 @@ class AppRoutes extends Component {
                 <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="lesson" element={<LessonDetails />} />
+                    <Route path='news-create' element={<CreateNews />} />
                 </Routes>
             </BrowserRouter>
         )
